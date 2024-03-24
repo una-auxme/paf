fix: optimized the kalman Filter with rolling average add on

code/perception/src/kalman_filter.py
@@ -14,7 +14,7 @@ from coordinate_transformation import CoordinateTransformer
 from coordinate_transformation import quat_to_heading
 from xml.etree import ElementTree as eTree
 
-GPS_RUNNING_AVG_ARGS: int = 10
+GPS_RUNNING_AVG_ARGS: int = 5
 
 '''
 This class implements a Kalman filter for a 3D object tracked in 3D space.
@@ -104,6 +104,9 @@ class KalmanFilter(CompatibleNode):
         self.x_pred = np.zeros((6, 1))  # Predicted state vector
         self.P_pred = np.zeros((6, 6))  # Predicted state covariance matrix
 
+        # Define Rolling Average for Kalman Output
+        self.avg_xyz = np.zeros((GPS_RUNNING_AVG_ARGS, 3))
+
         # Define state transition matrix
         '''
         # [x                ...             ]
@@ -148,7 +151,7 @@ class KalmanFilter(CompatibleNode):
         # The process covariance matrix Q is defined as:
         self.Q = np.diag([0.0001, 0.0001, 0.00001, 0.00001, 0.000001, 0.00001])
         # The measurement covariance matrix R is defined as:
-        self.R = np.diag([0.0005, 0.0005, 0, 0, 0, 0])
+        self.R = np.diag([0.0007, 0.0007, 0, 0, 0, 0])
 
         # self.x_old_est = np.copy(self.x0)  # old state vector
         # self.P_old_est = np.copy(self.P0)  # old state covariance matrix
@@ -286,6 +289,14 @@ class KalmanFilter(CompatibleNode):
         """
         Publish the kalman location
         """
+        kalman_x = self.x_est[0, 0]
+        kalman_y = self.x_est[1, 0]
+
+        self.avg_xyz = np.roll(self.avg_xyz, -1, axis=0)
+        self.avg_xyz[-1] = np.matrix([kalman_x, kalman_y, self.latitude])
+
+        avg_x, avg_y, avg_z = np.mean(self.avg_xyz, axis=0)
+
         # Initialize the kalman-position
         kalman_position = PoseStamped()
 
@@ -296,10 +307,15 @@ class KalmanFilter(CompatibleNode):
 
         self.publish_seq.data += 1
 
-        kalman_position.pose.position.x = self.x_est[0, 0]
-        kalman_position.pose.position.y = self.x_est[1, 0]
+        # kalman_position.pose.position.x = self.x_est[0, 0]
+        # kalman_position.pose.position.y = self.x_est[1, 0]
 
-        kalman_position.pose.position.z = self.latitude
+        # kalman_position.pose.position.z = self.latitude
+
+        kalman_position.pose.position.x = avg_x
+        kalman_position.pose.position.y = avg_y
+        kalman_position.pose.position.z = avg_z
+
         kalman_position.pose.orientation.x = 0
         kalman_position.pose.orientation.y = 0
         kalman_position.pose.orientation.z = 1