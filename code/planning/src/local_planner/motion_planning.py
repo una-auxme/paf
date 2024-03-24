83bf841e527fdc4eba51ed6f5f1be5196892564e
samuelkuehnel <samuel.kuehnel@gmail.com>
2024-03-24 14:04:05 +0100

    fix: Overtake trajecotry length bug

--- code/planning/src/local_planner/motion_planning.py
+++ code/planning/src/local_planner/motion_planning.py
@@ -240,7 +240,8 @@ class MotionPlanning(CompatibleNode):
         currentwp = self.current_wp
         normal_x_offset = 2
         unstuck_x_offset = 3  # could need adjustment with better steering
-        selection = pose_list[int(currentwp):int(currentwp) +
+        selection = pose_list[int(currentwp) + int(distance / 2):
+                              int(currentwp) +
                               int(distance) + NUM_WAYPOINTS]
         waypoints = self.convert_pose_to_array(selection)
 
