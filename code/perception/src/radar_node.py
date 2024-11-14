d537803e1b86b44418f326c6fac171b20782c1e0
Ralf <ralf.bernitt@uni-a.de>
2024-11-14 12:05:39 +0100

    update radar node

--- code/perception/src/radar_node.py
+++ code/perception/src/radar_node.py
@@ -10,6 +10,8 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import Image as ImageMsg
 from cv_bridge import CvBridge
 
+from std_msgs.msg import Float32
+
 # from matplotlib.colors import LinearSegmentedColormap
 
 
@@ -28,29 +30,53 @@ class RadarNode:
 
         :param data: a PointCloud2
         """
-        coordinates = ros_numpy.point_cloud2.pointcloud2_to_array(data)
+        # coordinates = ros_numpy.point_cloud2.pointcloud2_to_array(data)
 
         # Center
-        reconstruct_bit_mask_center = lidar_filter_utility.bounding_box(
-            coordinates,
-            max_x=np.inf,
-            min_x=0.0,
-            min_z=-1.6,
-        )
-        reconstruct_coordinates_center = coordinates[reconstruct_bit_mask_center]
-        reconstruct_coordinates_xyz_center = np.array(
-            lidar_filter_utility.remove_field_name(
-                reconstruct_coordinates_center, "intensity"
-            ).tolist()
-        )
-        dist_array_center = self.reconstruct_img_from_lidar(
-            reconstruct_coordinates_xyz_center, focus="Center"
-        )
-        dist_array_center_msg = self.bridge.cv2_to_imgmsg(
-            dist_array_center, encoding="passthrough"
-        )
-        dist_array_center_msg.header = data.header
-        self.dist_array_center_publisher.publish(dist_array_center_msg)
+
+        # reconstruct_bit_mask_center = lidar_filter_utility.bounding_box(
+        #     coordinates,
+        #     max_x=np.inf,
+        #     min_x=0.0,
+        #     min_z=-1.6,
+        # )
+        # reconstruct_coordinates_center = coordinates[reconstruct_bit_mask_center]
+        # reconstruct_coordinates_xyz_center = np.array(
+        #     lidar_filter_utility.remove_field_name(
+        #         reconstruct_coordinates_center, "intensity"
+        #     ).tolist()
+        # )
+        # dist_array_center = self.reconstruct_img_from_lidar(
+        #     reconstruct_coordinates_xyz_center, focus="Center"
+        # )
+        # dist_array_center_msg = self.bridge.cv2_to_imgmsg(
+        #     dist_array_center, encoding="passthrough"
+        # )
+        # dist_array_center_msg.header = data.header
+        # self.dist_array_center_publisher.publish(dist_array_center_msg)
+
+        coordinates = ros_numpy.point_cloud2.pointcloud2_to_array(data)
+
+        # x_values = coordinates['x']
+        # rospy.loginfo("Erste 5 x Werte: {x_values[:5]}")
+
+        # depth_values = coordinates['depth']
+        # rospy.loginfo("Erste 5 Depth Werte: {depth_values[:5]}")
+
+        dtype_info = "\n".join([f"Feld '{name}': {coordinates.dtype[name]}" for name in coordinates.dtype.names])
+
+        # rospy.loginfo("DatentypenNeu: " + dtype_info) # funktioniert
+
+        # rospy.loginfo("DatentypenAlt: " + coordinates.dtype)
+
+        
+
+        msg = np.min[coordinates['Range']]
+
+        dtype_msginfo = ["{type(msg).__name__}"]
+        rospy.loginfo("DatentypenMsg: " + dtype_msginfo)
+
+        self.dist_array_radar_publisher.publish(msg)      
 
     def listener(self):
         """
@@ -70,16 +96,18 @@ class RadarNode:
         )
 
         # publisher for dist_array
-        self.dist_array_center_publisher = rospy.Publisher(
-            rospy.get_param("~image_distance_topic", "/paf/hero/Center/dist_array"),
-            ImageMsg,
-            queue_size=10,
-        )
 
-        # publisher for dist_array
-        self.dist_array_center_publisher = rospy.Publisher(
-            rospy.get_param("~image_distance_topic", "/paf/hero/Center/dist_array"),
-            ImageMsg,
+        # self.dist_array_center_publisher = rospy.Publisher(
+        #     rospy.get_param("~image_distance_topic", "/paf/hero/Center/dist_array"),
+        #     ImageMsg,
+        #     queue_size=10,
+        # )
+
+        # publisher for radar dist_array
+        self.dist_array_radar_publisher = rospy.Publisher(
+            rospy.get_param("~image_distance_topic", "/paf/hero/Radar/array"),
+            # PointCloud2,
+            Float32,
             queue_size=10,
         )
 
