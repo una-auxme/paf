7ecd33e4d1f19a77d581331292543a3183b0c582
Ralf <ralf.bernitt@uni-a.de>
2024-11-18 17:08:25 +0100

    update radar node

--- code/perception/src/radar_node.py
+++ code/perception/src/radar_node.py
@@ -30,53 +30,29 @@ class RadarNode:
 
         :param data: a PointCloud2
         """
-        # coordinates = ros_numpy.point_cloud2.pointcloud2_to_array(data)
-
-        # Center
-
-        # reconstruct_bit_mask_center = lidar_filter_utility.bounding_box(
-        #     coordinates,
-        #     max_x=np.inf,
-        #     min_x=0.0,
-        #     min_z=-1.6,
-        # )
-        # reconstruct_coordinates_center = coordinates[reconstruct_bit_mask_center]
-        # reconstruct_coordinates_xyz_center = np.array(
-        #     lidar_filter_utility.remove_field_name(
-        #         reconstruct_coordinates_center, "intensity"
-        #     ).tolist()
-        # )
-        # dist_array_center = self.reconstruct_img_from_lidar(
-        #     reconstruct_coordinates_xyz_center, focus="Center"
-        # )
-        # dist_array_center_msg = self.bridge.cv2_to_imgmsg(
-        #     dist_array_center, encoding="passthrough"
-        # )
-        # dist_array_center_msg.header = data.header
-        # self.dist_array_center_publisher.publish(dist_array_center_msg)
 
         coordinates = ros_numpy.point_cloud2.pointcloud2_to_array(data)
-
         # x_values = coordinates['x']
         # rospy.loginfo("Erste 5 x Werte: {x_values[:5]}")
 
         # depth_values = coordinates['depth']
         # rospy.loginfo("Erste 5 Depth Werte: {depth_values[:5]}")
 
-        dtype_info = "\n".join([f"Feld '{name}': {coordinates.dtype[name]}" for name in coordinates.dtype.names])
-
+        # dtype_info = "\n".join([f"Feld '{name}': {coordinates.dtype[name]}" for name in coordinates.dtype.names])
         # rospy.loginfo("DatentypenNeu: " + dtype_info) # funktioniert
 
-        # rospy.loginfo("DatentypenAlt: " + coordinates.dtype)
+        #msg = np.min(coordinates['Velocity'])
+        # rospy.loginfo("DatentypenMsg: " + str(msg.dtype))
+        # rospy.loginfo("WertMsg: " + str(msg))
 
-        
+        #self.dist_array_radar_publisher.publish(msg)
 
-        msg = np.min[coordinates['Range']]
+        for coordinate in coordinates:
 
-        dtype_msginfo = ["{type(msg).__name__}"]
-        rospy.loginfo("DatentypenMsg: " + dtype_msginfo)
-
-        self.dist_array_radar_publisher.publish(msg)      
+            if coordinate['y'] == 0:
+                if coordinate['x'] <= 15:
+                    msg = coordinate['Velocity']*3.6
+                    self.dist_array_radar_publisher.publish(msg)
 
     def listener(self):
         """
@@ -85,23 +61,6 @@ class RadarNode:
         # run simultaneously.
         rospy.init_node("lidar_distance")
         self.bridge = CvBridge()
-
-        self.pub_pointcloud = rospy.Publisher(
-            rospy.get_param(
-                "~point_cloud_topic",
-                "/carla/hero/" + rospy.get_namespace() + "_filtered",
-            ),
-            PointCloud2,
-            queue_size=10,
-        )
-
-        # publisher for dist_array
-
-        # self.dist_array_center_publisher = rospy.Publisher(
-        #     rospy.get_param("~image_distance_topic", "/paf/hero/Center/dist_array"),
-        #     ImageMsg,
-        #     queue_size=10,
-        # )
 
         # publisher for radar dist_array
         self.dist_array_radar_publisher = rospy.Publisher(
