6be97bcc469e7775b6c5d13e3e5f675b43f40f81
Max Grimm <max.grimm2@gmail.com>
2024-11-19 16:52:59 +0000

    perception.launch die lanedetection_node mit debugger eingefügt + Model laden tests

---
+++ code/perception/src/Lanedetection_node.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+import pickle
+import os
+from ros_compatibility.node import CompatibleNode
+
+# from rospy.numpy_msg import numpy_msg
+# import rospy
+
+# import numpy as np
+import ros_compatibility as roscomp
+
+# import cv2
+# from sensor_msgs.msg import Image as ImageMsg
+# import torch
+# from cv_bridge import CvBridge
+
+
+class Lanedetection_node(CompatibleNode):
+    """CLRerNet:
+    Model for Lanedetection
+
+    subscribes to camera image and publishes lane masks for further planning
+    """
+
+    def __init__(self, name, **kwargs):
+        super().__init__(name, **kwargs)
+
+        model_path = os.path.join("CLRerNet_model", "data.pkl")
+        ws_path = os.path.dirname(os.path.realpath(__file__))
+        model_path2 = os.path.join(ws_path, model_path)
+        # model_path = "/home/paf/git/paf/code/perception/src/CLRerNet_model/data.pkl"
+
+        with open(model_path2, "rb") as file:
+            model = pickle.load(file)
+
+        print("Model erforgreich geladen:", model, model_path)
+
+        # general setup
+        """self.bridge = CvBridge()
+        self.role_name = self.get_param("role_name", "hero")
+        self.side = self.get_param("side", "Center")
+        self.center = self.get_param("center")
+        self.back = self.get_param("back")
+        self.left = self.get_param("left")
+        self.right = self.get_param("right")
+
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.model = None
+        self.weigths = None
+
+        # publish / subscribe setup
+        if self.center:
+            self.setup_camera_subscriptions("Center")
+        if self.back:
+            self.setup_camera_subscriptions("Back")
+        if self.left:
+            self.setup_camera_subscriptions("Left")
+        if self.right:
+            self.setup_camera_subscriptions("Right")
+
+        self.setup_mask_publisher()
+        """
+
+    def run(self):
+        self.spin()
+        pass
+
+    def setup_camera_subscriptions(self, side):
+        """
+        sets up a subscriber to the selected camera angle
+
+        Args:
+            side (String): Camera angle specified in launch file
+        """
+        """
+        self.new_subscription(
+            msg_type=numpy_msg(ImageMsg),
+            callback=self.handle_camera_image,
+            topic=f"/carla/{self.role_name}/{side}/image",
+            qos_profile=1,
+        )"""
+
+    def setup_mask_publisher(self):
+        """
+        sets up a publisher to the selected camera angle
+        if multiple cameras are enabled, there are multiple publishers used
+        """
+        """
+        if self.center:
+            self.publisher_center = self.new_publisher(
+                msg_type=numpy_msg(ImageMsg),
+                topic=f"/paf/{self.role_name}/Center/lane_mask",
+                qos_profile=1,
+            )
+        if self.back:
+            self.publisher_back = self.new_publisher(
+                msg_type=numpy_msg(ImageMsg),
+                topic=f"/paf/{self.role_name}/Back/lane_mask",
+                qos_profile=1,
+            )
+        if self.left:
+            self.publisher_left = self.new_publisher(
+                msg_type=numpy_msg(ImageMsg),
+                topic=f"/paf/{self.role_name}/Left/lane_mask",
+                qos_profile=1,
+            )
+        if self.right:
+            self.publisher_right = self.new_publisher(
+                msg_type=numpy_msg(ImageMsg),
+                topic=f"/paf/{self.role_name}/Right/lane_mask",
+                qos_profile=1,
+            )"""
+
+
+if __name__ == "__main__":
+    roscomp.init("Lanedetection_node")
+    node = Lanedetection_node("Lanedetection_node")
+    node.run()
