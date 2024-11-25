3b8c9527a280de053d7bf279d67ec0456e5847bc
Marcin <martink1998@gmail.com>
2024-11-25 11:27:52 +0100

    Lane Detection for the other teams this is a simple version.

--- code/perception/src/condl_resnet_101.py
+++ code/perception/src/condl_resnet_101.py
@@ -5,30 +5,139 @@ import ros_compatibility as roscomp
 from rospy.numpy_msg import numpy_msg
 from sensor_msgs.msg import Image as ImageMsg
 import rospy
+import torch
+import numpy as np
+import cv2
+import math
+from cv_bridge import CvBridge
+
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+
+def draw_lines(img, lines, color=[255, 0, 0], thickness=3):
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    img = np.copy(img)
+    rospy.loginfo(img)
+    rospy.loginfo(len(lines))
+    rospy.loginfo(type(img))
+
+    if lines is None:
+        return
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(line_img, (x1, y1), (x2, y2), color, thickness)
+    rospy.loginfo(type(line_img))
+    rospy.loginfo(img.shape)
+    rospy.loginfo(line_img.shape)
+    img = cv2.addWeighted(img, 0.8, line_img, 1.0, 0.0)
+    return img
 
 
 class LaneDetection(CompatibleNode):
 
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
+        self.bridge = CvBridge()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.new_subscription(
             msg_type=numpy_msg(ImageMsg),
             callback=self.handle_camera_image,
-            topic="/carla/hero/Center/image",
+            topic="/carla/hero/Front/image",
             qos_profile=1,
         )
 
-        self.traffic_light_publisher = self.new_publisher(
+        self.publisher = self.new_publisher(
             msg_type=numpy_msg(ImageMsg),
-            topic=f"/paf/hero/Lane_Detection_Mask",
+            topic="/paf/hero/Lane_Detection_Mask",
             qos_profile=1,
         )
 
     def run(self):
         self.spin()
 
-    def handle_camera_image(self, image):
-        rospy.loginfo("yhea")
+    def handle_camera_image(self, msg_image):
+        if self.device == "cuda":
+            torch.cuda.empty_cache()
+        image = self.bridge.imgmsg_to_cv2(img_msg=msg_image, desired_encoding="rgb8")
+        prediction = self.detection(image)
+
+        img_msg = self.bridge.cv2_to_imgmsg(prediction, encoding="rgb8")
+        img_msg.header = msg_image.header
+
+        self.publisher.publish(img_msg)
+
+    def detection(self, image):
+        height = image.shape[0]
+        width = image.shape[1]
+        region_of_interest_vertices = [
+            (0, height),
+            (width / 2, height / 2),
+            (width, height),
+        ]
+
+        gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        cannyed_image = cv2.Canny(gray_image, 100, 200)
+
+        cropped_image = region_of_interest(
+            cannyed_image,
+            np.array([region_of_interest_vertices], np.int32),
+        )
+
+        lines = cv2.HoughLinesP(
+            cropped_image,
+            rho=6,
+            theta=np.pi / 60,
+            threshold=160,
+            lines=np.array([]),
+            minLineLength=40,
+            maxLineGap=25,
+        )
+
+        left_line_x = []
+        left_line_y = []
+        right_line_x = []
+        right_line_y = []
+
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                slope = (y2 - y1) / (x2 - x1)
+                if math.fabs(slope) < 0.5:
+                    continue
+                if slope <= 0:
+                    left_line_x.extend([x1, x2])
+                    left_line_y.extend([y1, y2])
+                else:
+                    right_line_x.extend([x1, x2])
+                    right_line_y.extend([y1, y2])
+
+        min_y = int(image.shape[0] * (3 / 5))
+        max_y = int(image.shape[0])
+        poly_left = np.poly1d(np.polyfit(left_line_y, left_line_x, deg=1))
+
+        left_x_start = int(poly_left(max_y))
+        left_x_end = int(poly_left(min_y))
+
+        poly_right = np.poly1d(np.polyfit(right_line_y, right_line_x, deg=1))
+
+        right_x_start = int(poly_right(max_y))
+        right_x_end = int(poly_right(min_y))
+        line_image = draw_lines(
+            image,
+            [
+                [
+                    [left_x_start, max_y, left_x_end, min_y],
+                    [right_x_start, max_y, right_x_end, min_y],
+                ]
+            ],
+            thickness=5,
+        )
+        return line_image
 
 
 if __name__ == "__main__":
