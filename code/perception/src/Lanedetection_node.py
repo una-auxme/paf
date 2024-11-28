453ffe47498904a9d32230b041d9ecbdf0117bd3
lukasnol <lukas-nolle@web.de>
2024-11-28 12:12:40 +0100

    fixed model loading

--- code/perception/src/Lanedetection_node.py
+++ code/perception/src/Lanedetection_node.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 
-import os
 from ros_compatibility.node import CompatibleNode
 
 from rospy.numpy_msg import numpy_msg
 
 # import rospy
 
-# import numpy as np
+import numpy as np
 import ros_compatibility as roscomp
 
-# import cv2
+import cv2
 from sensor_msgs.msg import Image as ImageMsg
 from std_msgs.msg import Header
 
@@ -18,13 +17,8 @@ from cv_bridge import CvBridge
 
 # for the lane detection model
 import torch
-from CLRerNet_model.CLRerNet.configs.clrernet import base_clrernet as base_cfg
-from mmcv import Config
-from CLRerNet_model.CLRerNet.libs.models.detectors import clrernet as build_detector
-
-# for image preprocessing
-from torchvision import transforms
 from PIL import Image
+import torchvision.transforms as t
 
 
 class Lanedetection_node(CompatibleNode):
@@ -37,23 +31,10 @@ class Lanedetection_node(CompatibleNode):
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
 
-        # Config und Model laden
-        cfg = Config.fromfile(base_cfg)
-        self.model = build_detector(cfg.model, train_cfg=None, test_cfg=cfg.test)
+        # load model
+        self.model = torch.hub.load("hustvl/yolop", "yolop", pretrained=True)
 
-        # Gewichte laden
-        weight_path = os.path.join("CLRerNet_model", "clrernet_culane_dla34.pth")
-        ws_path = os.path.dirname(os.path.realpath(__file__))
-        ws_weight_path = os.path.join(ws_path, weight_path)
-
-        with open(ws_weight_path, "rb") as file:
-            self.state_dict = torch.load(file)
-
-        self.model.load_state_dict(self.state_dict)
-
-        # self.model.eval()
-        # print("Model erforgreich geladen:", self.model, model_path)
-
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.bridge = CvBridge()
         self.image_msg_header = Header()
         self.image_msg_header.frame_id = "segmented_image_frame"
@@ -82,6 +63,9 @@ class Lanedetection_node(CompatibleNode):
         )
 
     def setup_lane_publisher(self):
+        """sets up a publisher for the lane mask
+        topic: /Center/lane_img
+        """
         self.lane_publisher = self.new_publisher(
             msg_type=numpy_msg(ImageMsg),
             topic=f"/paf/{self.role_name}/Center/lane_img",
@@ -92,13 +76,25 @@ class Lanedetection_node(CompatibleNode):
         """
         Callback function for image subscriber
         """
+        # free up cuda memory
+        if self.device == "cuda":
+            torch.cuda.empty_cache()
 
-        image = self.bridge.imgmsg_to_cv2(ImageMsg, "bgr8")
-        image = self.preprocess_image(image)
+        image = self.preprocess_image(ImageMsg)
 
-        # lane_mask = self.detect_lanes(image, self.model)
+        det_out, da_seg_out, ll_seg_out = self.detect_lanes(image)
 
-        self.lane_publisher.publish(ImageMsg)
+        ll_seg_out = ll_seg_out.detach().cpu().numpy()  # Detach and move to CPU
+        # Ensure the tensor has the correct shape before transposing
+        if ll_seg_out.ndim == 4:  # Batch x Channels x H x W
+            ll_seg_out = ll_seg_out[0]  # Remove batch dimension
+        np_ll_img = np.transpose(ll_seg_out, (2, 0, 1))
+        ll_img = cv2.cvtColor(np_ll_img, cv2.COLOR_BGR2RGB)
+        # publish vision result to rviz
+        img_msg = self.bridge.cv2_to_imgmsg(ll_img, encoding="rgb8")
+        img_msg.header = image.header
+        # publish
+        self.lane_publisher.publish(img_msg)
 
     def preprocess_image(self, image):
         """
@@ -110,7 +106,7 @@ class Lanedetection_node(CompatibleNode):
         Returns:
             np.array: Preprocessed image
         """
-        pil_image = Image.fromarray(image)
+        """pil_image = Image.fromarray(image)
 
         preprocess = transforms.Compose(
             [
@@ -121,23 +117,41 @@ class Lanedetection_node(CompatibleNode):
                 ),
             ]
         )
+        return preprocess(pil_image).unsqueeze(0)
+        """
+        cv_image = self.bridge.imgmsg_to_cv2(img_msg=image, desired_encoding="rgb8")
+        cv_image = cv2.cvtColor(cv_image, cv2.COLOR_RGB2BGR)
+        pil_image = Image.fromarray(cv_image)
+        preprocess = t.Compose(
+            [
+                t.Resize((288, 800)),
+                t.ToTensor(),
+                t.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+        input_image = preprocess(pil_image).unsqueeze(dim=0)
+        return input_image
 
-        return preprocess(pil_image).unsqueeze(0).cuda()
-
-    def detect_lanes(self, image, model):
+    def detect_lanes(self, image):
         """
         Detects lanes in the image
 
         Args:
-            image (np.array): Image from camera
+            image (np.array): preprocessed Image from camera
 
         Returns:
             np.array: Lane mask
         """
-        with torch.no_grad():
-            output = model(image)
+        det_out, da_seg_out, ll_seg_out = self.model(image)
+        return det_out, da_seg_out, ll_seg_out
 
-        return output
+    def apply_mask(self, input_image, model_output):
+        """function to draw the lane mask into the original image
+
+        Args:
+            input_image (np.array): original input image
+            model_output (np.array): lane prediction
+        """
 
 
 if __name__ == "__main__":
