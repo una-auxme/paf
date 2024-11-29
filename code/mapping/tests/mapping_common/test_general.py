c3581c6c8b3676ea6929ac0052ac6b5f541937e0
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2024-11-29 16:47:13 +0100

    Add map conversion test

--- code/mapping/tests/mapping_common/test_general.py
+++ code/mapping/tests/mapping_common/test_general.py
@@ -1,7 +1,13 @@
-def test_bla():
-    pass
+from mapping_common.map import Map
+
+import test_entity
 
 
-def test_bla2():
-    assert 1 == 1
-    pass
+def test_map_conversion():
+    m = Map()
+    for i in range(200):
+        c = test_entity.get_car()
+        m.entities.append(c)
+    msg = m.to_ros_msg()
+    m_conv = Map.from_ros_msg(msg)
+    assert m == m_conv
