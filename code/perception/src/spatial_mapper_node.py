73afa6505cba8471d613604d50c778a317b97c4f
SirMDA <mail@ludwigholl.de>
2024-12-11 11:04:17 +0100

    wip

--- code/perception/src/spatial_mapper_node.py
+++ code/perception/src/spatial_mapper_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 from ros_compatibility.node import CompatibleNode
 import ros_compatibility as roscomp
 from vision_node_helper import carla_colors
@@ -92,21 +93,21 @@ class SpatialMapperNode(CompatibleNode):
         """
         Callback-Funktion zum Empfangen und Verarbeiten der Segmentierungsdaten
         """
-        try:
-            # Daten und Layout extrahieren
-            flat_data = np.array(msg.data, dtype=np.int8)
-            dimensions = [dim.size for dim in msg.layout.dim]
+        # try:
+        # Daten und Layout extrahieren
+        flat_data = np.array(msg.data, dtype=np.int8)
+        dimensions = [dim.size for dim in msg.layout.dim]
 
-            # Daten zurück in die ursprüngliche Form bringen
-            if len(dimensions) == 3:
-                segmentation_array = flat_data.reshape(dimensions)
-            else:
-                rospy.logwarn("Unexpected dimensions in segmentation mask")
+        # Daten zurück in die ursprüngliche Form bringen
+        if len(dimensions) == 3:
+            segmentation_array = flat_data.reshape(dimensions)
+        else:
+            rospy.logwarn("Unexpected dimensions in segmentation mask")
 
-            self.process_segmentation_mask(segmentation_array)
+        self.process_segmentation_mask(segmentation_array)
 
-        except Exception as e:
-            rospy.logerr(f"Error processing segmentation mask: {e}")
+        # except Exception as e:
+        #    rospy.logerr(f"Error processing segmentation mask: {e}")
 
     def process_segmentation_mask(self, segmentation_array):
         """
@@ -117,25 +118,42 @@ class SpatialMapperNode(CompatibleNode):
         # MarkerArray zum Sammeln aller Marker
         marker_array = MarkerArray()
 
-        marker_id = 0
-        for segmentation_mask in segmentation_array:
-            # Calculate depth values from self.dist_array
-            depth_array = self.calculate_depth_values(segmentation_mask)
+        for i, segmentation_mask in enumerate(segmentation_array):
+            if i == 0:
+                # Calculate depth values from self.dist_array
+                class_value = np.unique(segmentation_mask)
+                class_value = class_value[class_value != 0]
+                assert class_value.size == 1
+                class_value = class_value[0]
+                depth_array = self.calculate_depth_values(segmentation_mask)
 
-            # Linken, Rechten und Nähsten Punkt bekommen
-            left_point, right_point, nearest_point = self.extract_percentile_points(
-                segmentation_mask, depth_array
-            )
+                # Linken, Rechten und Nähsten Punkt bekommen
+                left_point, right_point, nearest_point = self.extract_percentile_points(
+                    segmentation_mask, depth_array
+                )
 
-            # Marker für diese Klasse erstellen
-            # point = np.random.uniform(-5, 5, size=3)  # Zufällige Position im Raum
-            marker = self.get_marker(point=left_point, id=marker_id, obj_class=1)
-            marker_array.markers.append(marker)
-            marker = self.get_marker(point=right_point, id=marker_id + 1, obj_class=2)
-            marker_array.markers.append(marker)
-            marker = self.get_marker(point=nearest_point, id=marker_id + 2, obj_class=4)
-            marker_array.markers.append(marker)
-            marker_id += 3
+                # Marker für Objekt erstellen
+                left_marker = self.get_marker(left_point, 0.1, 0.1, 0, 0, 1)
+                right_marker = self.get_marker(right_point, 0.1, 0.1, 0, 1, 1)
+                nearest_marker = self.get_marker(nearest_point, 0.1, 0.1, 0, 2, 1)
+
+                marker_array.markers.append(left_marker)
+                marker_array.markers.append(right_marker)
+                marker_array.markers.append(nearest_marker)
+
+                object_point, depth, width, rotation = self.calculate_object_marker(
+                    left_point, right_point, nearest_point
+                )
+
+                marker = self.get_marker(
+                    point=object_point,
+                    depth=depth,
+                    width=width,
+                    rotation=rotation,
+                    id=i,
+                    obj_class=class_value,
+                )
+                marker_array.markers.append(marker)
 
         # MarkerArray veröffentlichen
         try:
@@ -144,7 +162,7 @@ class SpatialMapperNode(CompatibleNode):
         except Exception as e:
             rospy.logerr(f"Error publishing markers: {e}")
 
-    def get_marker(self, point, id, obj_class):
+    def get_marker(self, point, depth, width, rotation, id, obj_class):
         c_color = carla_colors[obj_class]
         marker = Marker()
         marker.header.frame_id = "hero"
@@ -153,20 +171,18 @@ class SpatialMapperNode(CompatibleNode):
         marker.id = id
         marker.type = Marker.CUBE
         marker.action = Marker.ADD
-        marker.scale.x = 0.1
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
+        marker.scale.x = depth
+        marker.scale.y = width
+        marker.scale.z = 1
         marker.color.a = 1.0
         marker.color.r = c_color[0]
         marker.color.g = c_color[1]
         marker.color.b = c_color[2]
-        marker.pose.orientation.x = 0.0
-        marker.pose.orientation.y = 0.0
-        marker.pose.orientation.z = 0.0
-        marker.pose.orientation.w = 1.0
+        marker.pose.orientation.z = math.sin(rotation / 2)
+        marker.pose.orientation.w = math.cos(rotation / 2)
         marker.pose.position.x = point[0]
         marker.pose.position.y = point[1]
-        marker.pose.position.z = -point[2]
+        marker.pose.position.z = 0
         marker.lifetime = rospy.Duration(0.2)
         return marker
 
@@ -189,22 +205,19 @@ class SpatialMapperNode(CompatibleNode):
     def extract_percentile_points(
         self, mask_array, depth_array, percent=0.2, depth_threshold=1
     ):
-        # Get closest point
-        non_zero_values = depth_array[depth_array > 0]
-        if len(non_zero_values) == 0:
-            return [], [], []
-        min_value = np.min(non_zero_values)
-        closest_point_index = np.where(depth_array == min_value)
-        closest_point = self.dist_array[closest_point_index][0]
-
-        # Get left and right points
         valid_mask = depth_array > 0
-        non_zero_mask = ~np.all(
-            self.dist_array == [0.0, 0.0, 0.0], axis=2
+        non_zero_mask = (
+            ~(self.dist_array[:, :, 0] == 0.0)
+            & ~(self.dist_array[:, :, 1] == 0.0)
+            & ~(self.dist_array[:, :, 2] == 0.0)
         )  # Prüfe entlang der dritten Dimension
         combined_mask = valid_mask & non_zero_mask
         valid_points = self.dist_array[combined_mask]
         valid_depths = depth_array[combined_mask]
+        min_index = np.argmin(valid_depths)
+        closest_point = valid_points[min_index]
+
+        # Get left and right points
         sorted_indices = np.argsort(valid_points[:, 1])  # y-Wert ist die zweite Spalte
         sorted_points = valid_points[sorted_indices]
         sorted_depths = valid_depths[sorted_indices]
@@ -226,17 +239,39 @@ class SpatialMapperNode(CompatibleNode):
 
         left_significant_change_idx = np.where(left_depth_diffs > depth_threshold)[0]
         if len(left_significant_change_idx) > 0:
-            left_boundary_idx = (
-                left_significant_change_idx[0] - 1
-            )  # +1, da Differenz sich auf den nächsten Punkt bezieht
+            left_boundary_idx = left_significant_change_idx[
+                0
+            ]  # -1, da Differenz sich auf den nächsten Punkt bezieht
         else:
             left_boundary_idx = n - 1
-
+        assert left_boundary_idx >= 0 and left_boundary_idx < n
+        assert right_boundary_idx >= 0 and right_boundary_idx < n
         return (
             left_n_points[left_boundary_idx],
             right_n_points[right_boundary_idx],
             closest_point,
         )
+
+    def calculate_object_marker(self, left, right, nearest):
+        x_l, y_l, _ = left
+        x_r, y_r, _ = right
+        x_n, y_n, _ = nearest
+        # Breite
+        width = math.sqrt((x_r - x_l) ** 2 + (y_r - y_l) ** 2)
+
+        # Tiefe
+        depth = abs(
+            (y_r - y_l) * x_n - (x_r - x_l) * y_n + x_r * y_l - y_r * x_l
+        ) / math.sqrt((y_r - y_l) ** 2 + (x_r - x_l) ** 2)
+
+        # Rotation
+        rotation = math.atan2(y_r - y_l, x_r - x_l)
+
+        # Mittelpunkt
+        midpoint_x = (x_l + x_r) / 2
+        midpoint_y = (y_l + y_r) / 2
+
+        return [midpoint_x, midpoint_y, 0], depth, width, rotation
 
 
 if __name__ == "__main__":
