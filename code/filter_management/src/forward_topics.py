305b404c7d068630ba1033095a68a7c2f2952105
Antonia Gesswein <antonia@gesswein.net>
2025-01-22 12:54:35 +0100

    Forward Odometry msg from navsat node and IMU data to the EKF node

--- code/filter_management/src/forward_topics.py
+++ code/filter_management/src/forward_topics.py
@@ -3,6 +3,7 @@
 import ros_compatibility as roscomp
 from ros_compatibility.node import CompatibleNode
 from sensor_msgs.msg import NavSatFix, Imu
+from nav_msgs.msg import Odometry
 import threading
 
 """
@@ -40,13 +41,31 @@ class ForwardTopics(CompatibleNode):
             self.forward_gps_data,
             qos_profile=1,
         )
+        # Initialize the subscriber for the Odometry message
+        # between the navsat and ekf node
+        self.odom_subscriber = self.new_subscription(
+            Odometry,
+            "/odometry/gps",
+            self.forward_odom_data,
+            qos_profile=1,
+        )
 
         # Publisher
 
-        # Initialize the publisher for the IMU data
-        self.imu_publisher = self.new_publisher(Imu, "/imu/data", qos_profile=1)
+        # Initialize the publishers for the IMU data
+        self.imu_to_navsat_publisher = self.new_publisher(
+            Imu, "/imu/data", qos_profile=1
+        )
+        self.imu_to_ekf_publisher = self.new_publisher(
+            Imu, "/example/imu", qos_profile=1
+        )
         # Initialize the publisher for the GPS data
         self.gps_publisher = self.new_publisher(NavSatFix, "/gps/fix", qos_profile=1)
+        # Initialize the publisher for the Odometry message
+        # between the navsat and ekf node
+        self.odom_publisher = self.new_publisher(
+            Odometry, "/example/odom", qos_profile=1
+        )
 
     def run(self):
         self.loginfo("Forward Topics node running")
@@ -62,10 +81,14 @@ class ForwardTopics(CompatibleNode):
         self.spin()
 
     def forward_imu_data(self, imu_data):
-        self.imu_publisher.publish(imu_data)
+        self.imu_to_navsat_publisher.publish(imu_data)
+        self.imu_to_ekf_publisher.publish(imu_data)
 
     def forward_gps_data(self, gps_data):
         self.gps_publisher.publish(gps_data)
+
+    def forward_odom_data(self, odom_data):
+        self.odom_publisher.publish(odom_data)
 
 
 def main(args=None):
