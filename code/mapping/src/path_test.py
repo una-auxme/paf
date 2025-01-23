3a0f48cc7abab96c9462d42585ab16bad156875e
Sebastian Seitz <sebastian.seitz1@tha.de>
2025-01-23 17:27:21 +0100

    test path is now dynamic and swiches sides

--- code/mapping/src/path_test.py
+++ code/mapping/src/path_test.py
@@ -6,6 +6,7 @@ from ros_compatibility.node import CompatibleNode
 from nav_msgs.msg import Path
 from mapping.msg import Map as MapMsg
 from mapping_common.map import Map
+import random
 import shapely
 
 
@@ -28,68 +29,37 @@ class TestPath(CompatibleNode):
             qos_profile=1,
         )
 
-        self.current_pos_subscriber = self.new_subscription(
-            Path,
-            "/test_trajectory",
-            self.callback,
-            qos_profile=1,
-        )
+        self.rate = 0.5
+        self.new_timer(0.5, self.generate_trajectory)
 
-        self.new_subscription(
-            topic=self.get_param("~map_topic", "/paf/hero/mapping/init_data"),
-            msg_type=MapMsg,
-            callback=self.map_callback,
-            qos_profile=1,
-        )
+    def generate_trajectory(self, timer_event=None) -> Path:
+        path_msg = Path()
+        path_msg.header = rospy.Header()
+        path_msg.header.stamp = rospy.Time.now()
+        path_msg.header.frame_id = "hero"
+        path_msg.poses = []
+        curve = 0
+        factor = 1
+        if random.uniform(-1, 1) < 0:
+            factor = -1
 
-    def run(self):
-        self.loginfo("TestPath node running")
+        for i in range(0, 10):
+            pos = PoseStamped()
+            pos.header.stamp = rospy.Time.now()
+            pos.header.frame_id = "hero"
+            curve = curve + 0.075 * i * random.uniform(0, 1)
+            pos.pose.position.x = i
+            pos.pose.position.y = curve * factor
+            pos.pose.position.z = 0
+            pos.pose.orientation.w = 1
+            path_msg.poses.append(pos)
 
-        def loop(timer_event=None):
-            pose_stamps = []
-            for i in range(0, 10):
-                temp = PoseStamped()
-                temp.pose.position.x = i / 2
-                temp.pose.position.y = i * 0.2
-                temp.header.seq = i
-                temp.header.stamp = roscomp.ros_timestamp(
-                    self.get_time(), from_sec=True
-                )
-                temp.header.frame_id = "hero"
-                pose_stamps.append(temp)
-
-            msg = Path()
-            msg.poses = pose_stamps
-            msg.header.stamp = roscomp.ros_timestamp(self.get_time(), from_sec=True)
-            self.publisher.publish(msg)
-
-        self.new_timer(5, loop)
-        self.spin()
-
-    def callback(self, data: Path):
-        self.local_trajectory = data
-
-    def map_callback(self, data: MapMsg):
-        map = Map.from_ros_msg(data)
-        status = map.check_trajectory(self.local_trajectory)
-        self.loginfo("#Status trajectory check " + str(status))
-
-
-def main(args=None):
-    """
-    Main function starts the node
-    :param args:
-    """
-    roscomp.init("main_frame_publisher", args=args)
-
-    try:
-        node = TestPath()
-        node.run()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        roscomp.shutdown()
+        self.local_trajectory = path_msg
+        self.publisher.publish(path_msg)
 
 
 if __name__ == "__main__":
-    main()
+    name = "path_test_node"
+    roscomp.init(name)
+    node = TestPath()
+    node.spin()
