fixed vector calculation in radarnode, added debug prints in mappingnode

code/mapping/src/mapping_data_integration.py
@@ -320,6 +320,19 @@ class MappingDataIntegrationNode(CompatibleNode):
                 f"Mappingnode type(motionArray[0] nachher: {type(motion_array_converted[0])}"
             )
             # rospy.loginfo(
+            #     f"Mappingnode type(speed): {type(self.hero_speed)}"
+            # )  # carlaSpeedometer
+            # cluster_motion = motion_array_converted[indexarray == 0]
+            # motion = cluster_motion[0]
+            # rospy.loginfo(
+            #     f"Mappingnode type(motion.linear_motion): {type(motion.linear_motion)}"
+            # )
+            if self.hero_speed is not None:
+                motion_vector_hero = Vector2.forward() * self.hero_speed.speed
+                rospy.loginfo(
+                    f"Mappingnode type(speedvector): {type(motion_vector_hero)}"
+                )  # Vector2
+            # rospy.loginfo(
             #     f"Datentyp motion array converted vorher: {type(motion_array_converted)}"
             # ) # list
             motion_array_converted = np.array(motion_array_converted)
@@ -383,10 +396,35 @@ class MappingDataIntegrationNode(CompatibleNode):
                 # motion = motion_array_converted[0]
                 cluster_motion = motion_array_converted[indexarray == label]
                 motion = cluster_motion[0]
-                # rospy.loginfo("motion_array_converted und zugewiesen")
                 rospy.loginfo(
-                    f"motion_array_converted linear: {motion.angular_velocity}"
+                    f"Mappingnode type(motion.linear_motion): {type(motion.linear_motion)}"
+                )  # Vector2
+                # rospy.loginfo(f"type motion: {type(motion)}")  # Motion2D
+                # if self.hero_speed is not None:
+                if self.hero_speed is not None:
+                    motion_vector_hero = Vector2.forward() * self.hero_speed.speed
+                    relative_motion = motion.linear_motion - motion_vector_hero
+                    rospy.loginfo(
+                        f"Relative motion: x={relative_motion.x()}, y={relative_motion.y()}"
+                    )
+                    motion = Motion2D(relative_motion, angular_velocity=0.0)
+
+                rospy.loginfo(
+                    f"Relative motion: x={motion.linear_motion.x()}, y={motion.linear_motion.y()}, angular={motion.angular_velocity}"
                 )
+
+                # motion.linear_velocity = motion.linear_velocity.__sub__(
+                #     motion_vector_hero
+                # )
+                # rospy.loginfo(f"type motion_vector_hero: {type(motion_vector_hero)}")
+                # rospy.loginfo(f"type motion_.linear: {type(motion.linear_velocity)}")
+                # rospy.loginfo("motion_array_converted und zugewiesen")
+                # rospy.loginfo(
+                #     f"motion_array_converted angular: {motion.angular_velocity}"
+                # )
+                # rospy.loginfo(
+                #     f"motion_array_converted linear: {motion.linear_velocity.x}"
+                # )
             # rospy.loginfo(f"Len von indexarray: {len(indexarray)}")
             # rospy.loginfo(f"Len von motionarray: {len(motion_array_converted)}")
 

code/perception/src/radar_node.py
@@ -754,19 +754,21 @@ def calculate_cluster_velocity(points_with_labels):
         cluster_points = points_with_labels[points_with_labels[:, -1] == label]
 
         avg_velocity = np.mean(cluster_points[:, 3])
-        avg_x = np.mean(cluster_points[:, 0])
-        avg_y = np.mean(cluster_points[:, 1])
+        # avg_x = np.mean(cluster_points[:, 0])
+        # avg_y = np.mean(cluster_points[:, 1])
 
         # Normalisiere die Richtung (x, y)
-        magnitude = np.sqrt(avg_x**2 + avg_y**2)
-        if magnitude == 0:
-            direction = np.array([0, 0])  # Keine Richtung
-        else:
-            direction = np.array([avg_x, avg_y]) / magnitude
+        # magnitude = np.sqrt(avg_x**2 + avg_y**2)
+        # if magnitude == 0:
+        #     direction = np.array([0, 0])  # Keine Richtung
+        # else:
+        #     direction = np.array([avg_x, avg_y]) / magnitude
 
         # Skaliere Richtung mit Geschwindigkeit
-        motion = avg_velocity * direction
-        cluster_motion = Vector2.new(motion[0], motion[1])
+        # motion = avg_velocity * direction
+        # motion = avg_velocity * avg_x
+        # cluster_motion = Vector2.new(motion[0], motion[1])
+        cluster_motion = Vector2.new(avg_velocity, 0.0)
         cluster_motions.append(Motion2D(cluster_motion, angular_velocity=0.0))
 
     motion_array = np.empty(len(points_with_labels), dtype=object)