2d0d0157a863e374a0c841fceaf9b066d81374a0
lukasnol <lukas-nolle@web.de>
2025-02-03 14:57:23 +0100

    fixed a bug

--- code/mapping/ext_modules/mapping_common/filter.py
+++ code/mapping/ext_modules/mapping_common/filter.py
@@ -9,6 +9,7 @@ from .map import Map
 from .entity import ShapelyEntity, Entity, FlagFilter
 from .shape import Polygon, Shape2D
 from .transform import Transform2D
+from rospy import loginfo
 
 
 class MapFilter:
@@ -53,7 +54,7 @@ class LaneIndexFilter(MapFilter):
         other_entities = map.filtered(other_f)
 
         intersections = map.get_lane_y_axis_intersections(direction="both")
-        y_values = [(uuid, intersections[uuid]) for uuid in intersections]
+        y_values = [(uuid, intersections[uuid][1]) for uuid in intersections]
         # separate negative and positive values
         positive_y = sorted(
             [(uuid, y) for uuid, y in y_values if y > 0], key=lambda x: x[1]
