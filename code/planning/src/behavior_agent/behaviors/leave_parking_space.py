2c999787ca2fa39288bf5fbc88564842f5afadb3
Johannes1098 <36966307+Johannes1098@users.noreply.github.com>
2025-02-10 10:03:53 +0100

    check method to pakring space

--- code/planning/src/behavior_agent/behaviors/leave_parking_space.py
+++ code/planning/src/behavior_agent/behaviors/leave_parking_space.py
@@ -118,7 +118,10 @@ class LeaveParkingSpace(py_trees.behaviour.Behaviour):
                     # otherwise pause unparking
                     tree = map.build_tree(mapping_common.map.lane_free_filter())
                     if map.entities and tree.is_lane_free(
-                        right_lane=False, lane_length=22.5, lane_transform=-5.0
+                        right_lane=False,
+                        lane_length=22.5,
+                        lane_transform=-5.0,
+                        check_method="rectangle",
                     ):
                         rospy.loginfo("Left lane is now free. Starting unparking.")
                         self.started = True
