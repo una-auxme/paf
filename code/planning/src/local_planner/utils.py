4cc779a6869a3ab78e071ac68f5dffa10a8d6732
Johannes1098 <36966307+Johannes1098@users.noreply.github.com>
2025-03-04 14:14:59 +0100

    utils distance commented out

--- code/planning/src/local_planner/utils.py
+++ code/planning/src/local_planner/utils.py
@@ -33,7 +33,7 @@ def convert_to_ms(speed: float):
     return speed / 3.6
 
 
-def get_distance(pos_1, pos_2):
+'''def get_distance(pos_1, pos_2):
     """Calculate the distance between two positions
 
     Args:
@@ -47,7 +47,7 @@ def get_distance(pos_1, pos_2):
     Returns:
         float: Distance
     """
-    return np.linalg.norm(pos_1 - pos_2)
+    return np.linalg.norm(pos_1 - pos_2)'''
 
 
 '''def spawn_car(distance):
