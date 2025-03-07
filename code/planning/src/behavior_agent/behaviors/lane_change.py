389d05dfd54b05d9e2541ee318f4cb72a2754af3
Johannes1098 <joha.fuch@gmx.de>
2025-03-07 15:55:33 +0000

    fix when bad distance is coming from waypoint / lanechange

--- code/planning/src/behavior_agent/behaviors/lane_change.py
+++ code/planning/src/behavior_agent/behaviors/lane_change.py
@@ -87,10 +87,18 @@ class Ahead(py_trees.behaviour.Behaviour):
         if lane_change is None:
             return debug_status(self.name, Status.FAILURE, "lane_change is None")
         else:
-            self.change_distance = lane_change.distance
             self.change_detected = lane_change.isLaneChange
             self.change_option = lane_change.roadOption
             self.change_position = lane_change.position
+            # get change distance from global change point (transfered to local
+            #  hero coords) as this is more accurate than lanechange msg distance
+            hero_transform = _get_global_hero_transform()
+            local_pos: Point2 = (
+                hero_transform.inverse()
+                * Point2.new(self.change_position.x, self.change_position.y)
+            )
+            trajectory_local = mapping_common.mask.ros_path_to_line(trajectory_local)
+            self.change_distance = trajectory_local.line_locate_point(local_pos.to_shapely())
 
         if (
             self.change_distance is None
@@ -113,6 +121,7 @@ class Ahead(py_trees.behaviour.Behaviour):
         if (
             self.change_detected
             and self.change_distance < TARGET_DISTANCE_TO_TRIGGER_LANECHANGE
+            and self.change_distance > 0
         ):
             # delete stop marks from overtake as they could block lane change
             update_stop_marks(
