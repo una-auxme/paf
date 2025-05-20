7d2d14af9b581045bb4a9e6530d0ae62af75f210
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-05-20 17:30:39 +0200

    Dockerfile work

--- build/docker/agent/setup-scripts/setup_user.sh
+++ build/docker/agent/setup-scripts/setup_user.sh
@@ -1,1 +1,6 @@
-echo "source ${VENV_DIR}/bin/activate" >>~/.bashrc
+#!/bin/bash
+set -e
+
+echo "source ${INTERNAL_VENV}/bin/activate" >>~/.bashrc
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >>~/.bashrc
+echo "source ${ROOT_ROS_WS}/install/local_setup.bash" >>~/.bashrc
