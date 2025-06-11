77ac8983e31d62f6e4871f9370e1b2a9d12fd496
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-06-11 19:14:19 +0200

    Apply suggestions

--- build/docker/carla/wait_for_carla.py
+++ build/docker/carla/wait_for_carla.py
@@ -6,11 +6,14 @@ def main():
     success = False
     while not success:
         try:
-            client = carla.Client(os.getenv("CARLA_SIM_HOST", "localhost"))
+            client = carla.Client(
+                os.getenv("CARLA_SIM_HOST", "localhost"),
+                int(os.getenv("CARLA_SIM_PORT", "2000")),
+            )
             client.set_timeout(0.5)
             print(f"Done waiting for carla. Version: {client.get_server_version()}")
             success = True
-        except BaseException as error:
+        except Exception as error:
             print(f"Waiting for carla: {error}")
 
 
