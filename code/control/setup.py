6c3192b9587fb17aa3b37ac257d848eb04482212
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-07-04 20:11:03 +0000

    Git mv acting and control

---
+++ code/control/setup.py
@@ -0,0 +1,26 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = "control"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="peter",
+    maintainer_email="peter.viechter@student.uni-augsburg.de",
+    description="TODO: Package description",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
