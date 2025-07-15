ef00da1467eaf5841f44c63713cfd1daee75870b
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-07-15 12:25:28 +0000

    Fix python user installation

--- build/docker/agent-ros2/scripts/entrypoint-dev.sh
+++ build/docker/agent-ros2/scripts/entrypoint-dev.sh
@@ -9,6 +9,14 @@ _term() {
 }
 trap _term SIGTERM SIGINT
 
+# Delete local python packages if they exist
+if [ -d ~/.local ]; then
+  rm -r ~/.local
+fi
+# Restore the home directory
+sudo mkdir -p /home
+sudo cp -ar --update=older /opt/home-bk/. -t /home/
+
 # Pull in the development bashrc
 # It does not get automatically loaded, because the entrypoint is not an interactive shell
 source "${INTERNAL_WORKSPACE_DIR}/dev.bashrc"
@@ -17,10 +25,6 @@ devbuild || echo "WARNING: Build failed, proceeding anyway..."
 
 echo "source ${PAF_ROS_WS}/install/local_setup.bash" >>"${INTERNAL_WORKSPACE_DIR}/env.bash"
 devsource || "WARNING: ROS workspace could not be sourced"
-
-# Restore the home directory
-sudo mkdir -p /home
-sudo cp -ar --update=older /opt/home-bk/. -t /home/
 
 ros2 run rqt_console rqt_console &
 ros2 run rqt_gui rqt_gui &
