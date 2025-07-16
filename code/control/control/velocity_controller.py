c53723035292e24603941ab840e00b8f49f396cd
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-07-16 14:10:48 +0000

    Port velocity controller

--- code/control/control/velocity_controller.py
+++ code/control/control/velocity_controller.py
@@ -1,151 +1,150 @@
-#!/usr/bin/env python
-import ros_compatibility as roscomp
+from typing import Optional
 from carla_msgs.msg import CarlaSpeedometer
-from ros_compatibility.node import CompatibleNode
-from rospy import Publisher, Subscriber
+import rclpy
+from rclpy.node import Node
+from rclpy.publisher import Publisher
+from rclpy.subscription import Subscription
 from simple_pid import PID
 from std_msgs.msg import Float32, Bool
-import rospy
-
-from dynamic_reconfigure.server import Server
-from control.cfg import VelocityConfig
 
 
-class VelocityController(CompatibleNode):
+class VelocityController(Node):
     """
     This node controls the velocity of the vehicle.
     For this it uses a PID controller.
     """
 
     def __init__(self):
-        super(VelocityController, self).__init__("velocity_controller")
-        self.loginfo("VelocityController node started")
+        super().__init__("velocity_controller")
+        self.get_logger().info("VelocityController node initializing...")
 
-        self.control_loop_rate = self.get_param("control_loop_rate", 0.05)
-        self.role_name = self.get_param("role_name", "ego_vehicle")
+        self.control_loop_rate_param = self.declare_parameter("control_loop_rate", 0.05)
+        self.control_loop_rate = (
+            self.control_loop_rate_param.get_parameter_value().double_value
+        )
+        self.role_name_param = self.declare_parameter("role_name", "ego_vehicle")
+        self.role_name = self.role_name_param.get_parameter_value().string_value
 
-        self.target_velocity_sub: Subscriber = self.new_subscription(
+        self.FIXED_SPEED_param = self.declare_parameter("fixed_speed", 0.0)
+        self.FIXED_SPEED_OVERRIDE_param = self.declare_parameter(
+            "fixed_speed_active", False
+        )
+
+        self.pid_p_param = self.declare_parameter("pid_p", 0.60)
+        self.pid_i_param = self.declare_parameter("pid_i", 0.00076)
+        self.pid_d_param = self.declare_parameter("pid_d", 0.63)
+
+        self.target_velocity_sub: Subscription = self.create_subscription(
             Float32,
             "/paf/acting/target_velocity",
             self.__get_target_velocity,
             qos_profile=1,
         )
 
-        self.velocity_sub: Subscriber = self.new_subscription(
+        self.velocity_sub: Subscription = self.create_subscription(
             CarlaSpeedometer,
             f"/carla/{self.role_name}/Speed",
             self.__get_current_velocity,
             qos_profile=1,
         )
 
-        self.throttle_pub: Publisher = self.new_publisher(
+        self.throttle_pub: Publisher = self.create_publisher(
             Float32, f"/paf/{self.role_name}/throttle", qos_profile=1
         )
 
-        self.brake_pub: Publisher = self.new_publisher(
+        self.brake_pub: Publisher = self.create_publisher(
             Float32, f"/paf/{self.role_name}/brake", qos_profile=1
         )
 
-        self.reverse_pub: Publisher = self.new_publisher(
+        self.reverse_pub: Publisher = self.create_publisher(
             Bool, f"/paf/{self.role_name}/reverse", qos_profile=1
         )
 
-        self.__current_velocity: float = None
-        self.__target_velocity: float = None
-        self.pid_t = PID(0.60, 0.00076, 0.63)
-
-        self.FIXED_SPEED_OVERRIDE: bool
-        self.FIXED_SPEED: float
-
-        Server(VelocityConfig, self.dynamic_reconfigure_callback)
-
-    def dynamic_reconfigure_callback(self, config: VelocityConfig, level):
-        self.pid_t.Kp = config["pid_p"]
-        self.pid_t.Ki = config["pid_i"]
-        self.pid_t.Kd = config["pid_d"]
-        self.FIXED_SPEED = config["fixed_speed"]
-        self.FIXED_SPEED_OVERRIDE = config["fixed_speed_active"]
-        return config
-
-    def run(self):
-        """
-        Starts the main loop of the node
-        :return:
-        """
-        self.loginfo("VelocityController node running")
-        # PID for throttle
-
+        self.__current_velocity: Optional[float] = None
+        self.__target_velocity: Optional[float] = None
+        self.pid_t = PID(
+            self.pid_p_param.get_parameter_value().double_value,
+            self.pid_i_param.get_parameter_value().double_value,
+            self.pid_d_param.get_parameter_value().double_value,
+        )
         # since we use this for braking aswell, allow -1 to 0.
         self.pid_t.output_limits = (-1.0, 1.0)
 
-        def loop(timer_event=None):
-            """
-            Calculates the result of the PID controller and publishes it.
-            Never publishes values above speed limit
-            (Publish speed_limit = -1 to drive without speeed limit)
-            :param timer_event: Timer event from ROS
-            :return:
-            """
-            if self.__target_velocity is None:
-                self.logdebug(
-                    "VelocityController hasn't received target"
-                    "_velocity yet. target_velocity has been set to"
-                    "default value 0"
-                )
-                self.__target_velocity = 0
+        self.loop_timer = self.create_timer(self.control_loop_rate, self.loop)
+        self.get_logger().info("VelocityController node initialized.")
 
-            if self.__current_velocity is None:
-                self.logdebug(
-                    "VelocityController  hasn't received "
-                    "current_velocity yet and can therefore not"
-                    "publish a throttle value"
-                )
-                return
-
-            target_velocity = (
-                self.__target_velocity
-                if not self.FIXED_SPEED_OVERRIDE
-                else self.FIXED_SPEED
+    def loop(self):
+        """
+        Calculates the result of the PID controller and publishes it.
+        Never publishes values above speed limit
+        (Publish speed_limit = -1 to drive without speeed limit)
+        :param timer_event: Timer event from ROS
+        :return:
+        """
+        if self.__target_velocity is None:
+            self.get_logger().debug(
+                "VelocityController hasn't received target"
+                "_velocity yet. target_velocity has been set to"
+                "default value 0"
             )
-            # revert driving
-            if target_velocity < 0:
-                reverse = True
-                v = abs(target_velocity)
-                self.pid_t.setpoint = v
-                brake = 0
-                throttle = self.pid_t(-self.__current_velocity)
-                if throttle < 0:
-                    brake = abs(throttle)
-                    throttle = 0
-            # very low target_velocities -> stand
-            elif target_velocity < 0.1:
-                reverse = False
-                brake = 1
+            self.__target_velocity = 0
+
+        if self.__current_velocity is None:
+            self.get_logger().debug(
+                "VelocityController  hasn't received "
+                "current_velocity yet and can therefore not"
+                "publish a throttle value"
+            )
+            return
+
+        self.pid_t.Kp = self.pid_p_param.get_parameter_value().double_value
+        self.pid_t.Ki = self.pid_i_param.get_parameter_value().double_value
+        self.pid_t.Kd = self.pid_d_param.get_parameter_value().double_value
+
+        FIXED_SPEED = self.FIXED_SPEED_param.get_parameter_value().double_value
+        FIXED_SPEED_OVERRIDE = (
+            self.FIXED_SPEED_OVERRIDE_param.get_parameter_value().bool_value
+        )
+
+        target_velocity = (
+            self.__target_velocity if not FIXED_SPEED_OVERRIDE else FIXED_SPEED
+        )
+        # revert driving
+        if target_velocity < 0:
+            reverse = True
+            v = abs(target_velocity)
+            self.pid_t.setpoint = v
+            brake = 0
+            throttle = self.pid_t(-self.__current_velocity)
+            if throttle < 0:
+                brake = abs(throttle)
+                throttle = 0
+        # very low target_velocities -> stand
+        elif target_velocity < 0.1:
+            reverse = False
+            brake = 1
+            throttle = 0
+        else:
+            reverse = False
+            v = target_velocity
+            self.pid_t.setpoint = v
+            throttle = self.pid_t(self.__current_velocity)
+            # any throttle < 0 is used as brake signal
+            if throttle < 0:
+                brake = abs(throttle)
                 throttle = 0
             else:
-                reverse = False
-                v = target_velocity
-                self.pid_t.setpoint = v
-                throttle = self.pid_t(self.__current_velocity)
-                # any throttle < 0 is used as brake signal
-                if throttle < 0:
-                    brake = abs(throttle)
-                    throttle = 0
-                else:
-                    brake = 0
+                brake = 0
 
-            self.reverse_pub.publish(reverse)
-            self.brake_pub.publish(brake)
-            self.throttle_pub.publish(throttle)
+        self.reverse_pub.publish(reverse)
+        self.brake_pub.publish(brake)
+        self.throttle_pub.publish(throttle)
 
-        def loop_handler(timer_event=None):
-            try:
-                loop()
-            except Exception as e:
-                rospy.logfatal(e)
-
-        self.new_timer(self.control_loop_rate, loop_handler)
-        self.spin()
+    def loop_handler(self):
+        try:
+            self.loop()
+        except Exception as e:
+            self.get_logger().fatal(e)
 
     def __get_current_velocity(self, data: CarlaSpeedometer):
         self.__current_velocity = float(data.speed)
@@ -159,15 +158,15 @@ def main(args=None):
     Main function starts the node
     :param args:
     """
-    roscomp.init("velocity_controller", args=args)
+    rclpy.init(args=args)
 
     try:
         node = VelocityController()
-        node.run()
+        rclpy.spin(node)
     except KeyboardInterrupt:
         pass
     finally:
-        roscomp.shutdown()
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
