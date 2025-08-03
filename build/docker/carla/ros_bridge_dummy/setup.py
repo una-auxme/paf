d1b672d840c29466f12bd6eaed5ae8297d10a812
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-08-03 21:32:55 +0200

    Rename carla launch config

--- build/docker/carla/ros_bridge_dummy/setup.py
+++ build/docker/carla/ros_bridge_dummy/setup.py
@@ -9,6 +9,7 @@ setup(
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name), glob(os.path.join("launch", "*.launch"))),
     ],
