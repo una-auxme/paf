e1a8f1d0a6bb6c888824521ba1d69414901b6dc6
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-09-16 17:43:43 +0200

    WIP port global planner node

--- code/planning/planning/global_planner/global_planner_node.py
+++ code/planning/planning/global_planner/global_planner_node.py
@@ -1,27 +1,28 @@
-#!/usr/bin/env python
 from collections import deque
 from typing import Deque
 from xml.etree import ElementTree as eTree
 
-import ros_compatibility as roscomp
-import rospy
-import tf.transformations
+import rclpy
+from rclpy.node import Node
+
+from transforms3d.euler import euler2quat
 from carla_msgs.msg import CarlaRoute  # , CarlaWorldInfo
 from geometry_msgs.msg import Point, Pose, PoseStamped, Quaternion
 from nav_msgs.msg import Path
-from ros_compatibility.node import CompatibleNode
 from std_msgs.msg import Float32MultiArray, String
 
 from global_planner.preplanning_trajectory import OpenDriveConverter
 
 from mapping_common.transform import Point2
 
+# TODO: These definition do not align with the CarlaRoute.RIGHT, etc.. definitions.
+# -> Check for possible bugs
 RIGHT = 1
 LEFT = 2
 FORWARD = 3
 
 
-class PrePlanner(CompatibleNode):
+class PrePlanner(Node):
     """
     This node is responsible for collecting all data needed for the
     preplanning and calculating a trajectory based on the OpenDriveConverter
@@ -37,7 +38,8 @@ class PrePlanner(CompatibleNode):
     """
 
     def __init__(self):
-        super(PrePlanner, self).__init__("DevGlobalRoute")
+        super().__init__(type(self).__name__)
+        self.get_logger().info(f"{type(self).__name__} node initializing...")
 
         self.path_backup = Path()
 
@@ -49,48 +51,57 @@ class PrePlanner(CompatibleNode):
         self.agent_pos = None
         self.agent_ori = None
 
-        self.role_name = self.get_param("role_name", "hero")
-        self.control_loop_rate = self.get_param("control_loop_rate", 1)
-        self.distance_spawn_to_first_wp = self.get_param(
-            "distance_spawn_to_first_wp", 100
+        self.role_name = (
+            self.declare_parameter("role_name", "hero")
+            .get_parameter_value()
+            .string_value
+        )
+        self.distance_spawn_to_first_wp = (
+            self.declare_parameter(
+                "distance_spawn_to_first_wp",
+                100.0,
+            )
+            .get_parameter_value()
+            .double_value
         )
 
-        self.map_sub = self.new_subscription(
+        self.map_sub = self.create_subscription(
             msg_type=String,
             topic=f"/carla/{self.role_name}/OpenDRIVE",
             callback=self.world_info_callback,
             qos_profile=10,
         )
 
-        self.global_plan_sub = self.new_subscription(
+        self.global_plan_sub = self.create_subscription(
             msg_type=CarlaRoute,
             topic="/carla/" + self.role_name + "/global_plan",
             callback=self._global_route_callback,
             qos_profile=10,
         )
 
-        self.current_pos_sub = self.new_subscription(
+        self.current_pos_sub = self.create_subscription(
             msg_type=PoseStamped,
             topic="/paf/" + self.role_name + "/current_pos",
             callback=self.position_callback,
             qos_profile=1,
         )
 
-        self.path_pub = self.new_publisher(
+        self.path_pub = self.create_publisher(
             msg_type=Path,
             topic="/paf/" + self.role_name + "/trajectory_global",
             qos_profile=1,
         )
 
-        self.speed_limit_pub = self.new_publisher(
+        self.speed_limit_pub = self.create_publisher(
             msg_type=Float32MultiArray,
             topic=f"/paf/{self.role_name}/speed_limits_OpenDrive",
             qos_profile=1,
         )
-        self.logdebug("PrePlanner-Node started")
 
         # uncomment for self.dev_load_world_info() for dev_launch
         # self.dev_load_world_info()
+
+        self.get_logger().info(f"{type(self).__name__} node initialized.")
 
     def _global_route_callback(self, data: CarlaRoute) -> None:
         self.update_global_route(data)
@@ -103,7 +114,7 @@ class PrePlanner(CompatibleNode):
         :param data: global Route
         """
         if self.odc is None:
-            self.logwarn(
+            self.get_logger().warn(
                 "PrePlanner: global route got updated before map... "
                 "therefore the OpenDriveConverter couldn't be "
                 "initialised yet"
@@ -112,7 +123,7 @@ class PrePlanner(CompatibleNode):
             return
 
         if self.agent_pos is None or self.agent_ori is None or data.poses is None:
-            self.logwarn(
+            self.get_logger().warn(
                 "PrePlanner: global route got updated before current "
                 "pose... therefore there is no pose to start with"
             )
@@ -127,7 +138,7 @@ class PrePlanner(CompatibleNode):
             abs(x_start - x_target) > self.distance_spawn_to_first_wp
             or abs(y_start - y_target) > self.distance_spawn_to_first_wp
         ):
-            self.logwarn(
+            self.get_logger().warn(
                 "PrePlanner: current agent-pose does not match the given global route"
             )
             self.global_route_backup = data
@@ -144,7 +155,7 @@ class PrePlanner(CompatibleNode):
                 ind = i
                 break
         if x_turn is None or y_turn is None:
-            self.logwarn("PrePlanner: Did not find first turn command")
+            self.get_logger().warn("PrePlanner: Did not find first turn command")
             self.global_route_backup = data
             return
         # if first target point is turning point
@@ -204,24 +215,24 @@ class PrePlanner(CompatibleNode):
         stamped_poses = []
         for i in range(len(way_x)):
             position = Point(way_x[i], way_y[i], 0)  # way_speed[i])
-            quaternion = tf.transformations.quaternion_from_euler(0, 0, way_yaw[i])
+            quaternion = euler2quat(0, 0, way_yaw[i])
             orientation = Quaternion(
-                x=quaternion[0], y=quaternion[1], z=quaternion[2], w=quaternion[3]
+                x=quaternion[1], y=quaternion[2], z=quaternion[3], w=quaternion[0]
             )
-            pose = Pose(position, orientation)
+            pose = Pose(position=position, orientation=orientation)
             pos = PoseStamped()
-            pos.header.stamp = rospy.Time.now()
+            pos.header.stamp = self.get_clock().now().to_msg()
             pos.header.frame_id = "global"
             pos.pose = pose
             stamped_poses.append(pos)
 
-        self.path_backup.header.stamp = rospy.Time.now()
+        self.path_backup.header.stamp = self.get_clock().now().to_msg()
         self.path_backup.header.frame_id = "global"
         self.path_backup.poses = stamped_poses
         self.path_pub.publish(self.path_backup)
 
         self.global_route_backup = None
-        self.logwarn("PrePlanner: published trajectory")
+        self.get_logger().info("PrePlanner: published trajectory")
 
     def world_info_callback(self, opendrive: String) -> None:
         """
@@ -229,7 +240,7 @@ class PrePlanner(CompatibleNode):
         (needed for the trajectory preplanning)
         :param opendrive: updated CarlaWorldInformation
         """
-        self.loginfo("PrePlanner: MapUpdate called")
+        self.get_logger().info("PrePlanner: MapUpdate called")
 
         if type(opendrive) is str:
             root = eTree.fromstring(opendrive)
@@ -255,7 +266,7 @@ class PrePlanner(CompatibleNode):
         self.odc = odc
 
         if self.global_route_backup is not None:
-            self.loginfo(
+            self.get_logger().info(
                 "PrePlanner: Received a map update -> retrying route preplanning"
             )
             self.update_global_route(self.global_route_backup)
@@ -314,25 +325,16 @@ class PrePlanner(CompatibleNode):
         self.logerr("DATA READ")
         self.world_info_callback(file_content)
 
-    def run(self):
-        """
-        Control loop
-        :return:
-        """
-        self.spin()
 
-
-if __name__ == "__main__":
-    """
-    main function starts the PrePlanner node
-    :param args:
-    """
-    roscomp.init("PrePlanner")
+def main(args=None):
+    rclpy.init(args=args)
 
     try:
         node = PrePlanner()
-        node.run()
+        rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    finally:
-        roscomp.shutdown()
+
+
+if __name__ == "__main__":
+    main()
