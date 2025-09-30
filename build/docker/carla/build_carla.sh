00146b79cfc855ca8673ef4532fd648a180bc74f
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-09-30 18:06:06 +0200

    WIP carla 0.9.16 container

--- build/docker/carla/build_carla.sh
+++ build/docker/carla/build_carla.sh
@@ -11,7 +11,7 @@ if [ ! -f ./carla-additional-maps.tar.gz ]; then
   wget --progress=bar https://tiny.carla.org/additional-maps-0-9-16-linux -O ./carla-additional-maps.tar.gz
 fi
 
-docker build --pull -t carla-leaderboard-gpu:2.1 --target carla -f ./Dockerfile ../../../ --progress=plain
+docker build --pull -t carla-leaderboard-gpu:2.1 --target carla -f ./Dockerfile ../../../
 docker build --pull -t carla-leaderboard-cuda:2.1 --target carla --build-arg BASE_FLAVOUR=cuda -f ./Dockerfile ../../../
 docker build --pull -t carla-leaderboard-api:2.1 --target carla-api -f ./Dockerfile ../../../
 docker build --pull -t carla-leaderboard-ros-bridge:2.1 --target carla-ros-bridge -f ./Dockerfile ../../../
