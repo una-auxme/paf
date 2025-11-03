d9ded926e732b57cdcc2a689d45722aa3c69d603
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-11-03 20:17:24 +0000

    Rework agent leaderboard starter files

---
+++ code/leaderboard_launcher/agent/agent.dev.py
@@ -0,0 +1,14 @@
+from . import agent
+
+
+def get_entry_point():
+    return "PAFAgentDev"
+
+
+class PAFAgentDev(agent.PAFAgent):
+
+    def get_ros_entrypoint(self):
+        return {
+            "package": "leaderboard_launcher",
+            "launch_file": "leaderboard.dev.xml",
+        }
