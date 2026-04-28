3f1e2ce378339036adafd649dfcec4ecfe91f10d
ll7 <sailingmaster92@gmx.de>
2026-04-28 10:32:08 +0200

    Fix formatting and add missing newline in launch_leaderboard.test.sh and lidar_distance.md

--- code/leaderboard_launcher/scripts/launch_leaderboard.test.sh
+++ code/leaderboard_launcher/scripts/launch_leaderboard.test.sh
@@ -17,7 +17,7 @@ traffic_manager_port="${PAF_TRAFFIC_MANAGER_PORT:-8000}"
 
 python3 "/workspace/code/leaderboard_launcher/leaderboard_launcher/wait_for_carla.py"
 
-# Start leaderboard with arguments # edit "--routes" if you want a different testroute 
+# Start leaderboard with arguments # edit "--routes" if you want a different testroute
 exec python3 /workspace/code/test/run_test.py \
   --host="${CARLA_SIM_HOST}" \
   --debug=0 \
